801b746747e89707cca46a0b5cf93e45c290f305
Steve Loewenstein <saloewenstein@my365.bellevue.edu>
2023-10-24 13:39:08 +0300

    stats

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,8 +20,8 @@ const Hero = async () => {
           MidJourney images
         </h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 ">
-          Reach millions of people through our partners and earn every time
-          someone uses your images.
+          Reach millions of people and earn every time
+          someone uses your images through our partners.
         </p>
 
         <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
--- components/UnderHero.tsx
+++ components/UnderHero.tsx
@@ -7,17 +7,25 @@ const UnderHero = () => {
             </p>
       <dl className="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
           <div className="flex flex-col items-center justify-center">
-              <dt className="mb-2 text-3xl md:text-4xl font-extrabold text-purple-600">1250+</dt>
-              <dd className="font-light text-gray-500 ">contributors</dd>
+              <dt className="mb-2 text-3xl md:text-4xl font-extrabold text-purple-600">10M+</dt>
+              <dd className="font-light text-gray-500 ">Images</dd>
           </div>
           <div className="flex flex-col items-center justify-center">
+              <dt className="mb-2 text-3xl md:text-4xl font-extrabold text-purple-600">25%</dt>
+              <dd className="font-light text-gray-500 ">Earned by Contributors</dd>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+              <dt className="mb-2 text-3xl md:text-4xl font-extrabold text-purple-600">$367,897</dt>
+              <dd className="font-light text-gray-500 ">Distributed</dd>
+          </div>
+          {/* <div className="flex flex-col items-center justify-center">
               <dt className="mb-2 text-3xl md:text-4xl font-extrabold text-purple-600">1M+</dt>
               <dd className="font-light text-gray-500 ">images</dd>
-          </div>
-          <div className="flex flex-col items-center justify-center">
+          </div> */}
+          {/* <div className="flex flex-col items-center justify-center">
               <dt className="mb-2 text-3xl md:text-4xl font-extrabold text-purple-600">20+</dt>
               <dd className="font-light text-gray-500 ">partners</dd>
-          </div>
+          </div> */}
       </dl>
   </div>
   )
